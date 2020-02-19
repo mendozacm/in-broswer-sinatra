6e959a9fe11df7e47843d672931a8f19a80699ac
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-02-19 01:02:56 +0000

    added a way to see the most recent post you made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,13 @@ class ApplicationController < Sinatra::Base
   helpers do
     
     def logged_in?
+      #checks to see if you're logged in
       !!current_user
     end
     
     
     def current_user
+      #defines the current user if the email is equal to session email
       @current_user ||= User.find_by(:email => session[:email]) if session[:email]
     end
   
@@ -35,7 +37,7 @@ class ApplicationController < Sinatra::Base
     end
       
   end
-
+  
   get "/" do
     erb :homepage
   end
@@ -48,6 +50,8 @@ class ApplicationController < Sinatra::Base
   post "/new_account_homepage" do
     erb :new_account_homepage
   end
+
+  
   
   
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,17 @@
 class PostsController < ApplicationController
   
   get '/posts' do
+    #posts that non users can see
    erb :public_posts
   end
   
   get '/posts/new' do
+    #checking to see if the user is logged in
     if !logged_in?
+      #if not logged in then redirect to the login page
       redirect "/login"
     else
+      #if they are logged in then redirect to 
       erb :new_post
     end
   end
@@ -25,14 +29,11 @@ class PostsController < ApplicationController
     end
   end
   
-  get'/new_posts/all_posts' do
-    if !logged_in?
-      redirect "/login"
-    else
-     "#{params[:dogs]}"
-       
-   end
- end
+  post '/new_posts/all_posts' do
+    erb :all_posts
+  end
+ 
+ 
   
   
   get '/posts/:id/edit' do
