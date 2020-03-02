d936a67ff88d258749fcc5af61a939f0cab4609e
Cesar Mendoza <mendoza.cesar92@gmail.com>
2020-03-02 06:02:39 +0000

    added a required field for the user input in the email and password fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
  has_secure_password
  
- validates :email, :presence => true
+ validates :email, :presence => true,
+                   :uniqueness => true
  
  has_many :posts
 end
